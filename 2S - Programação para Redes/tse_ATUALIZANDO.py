624e3ae05072085f1af5b9f753b54cedf0bb2864
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2023-04-15 23:52:25 -0300

    Update tse_ATUALIZANDO.py

--- 2S - Programação para Redes/tse_ATUALIZANDO.py
+++ 2S - Programação para Redes/tse_ATUALIZANDO.py
@@ -80,21 +80,48 @@ while True:
 
 
 
-
 url  = 'https://resultados.tse.jus.br/oficial/'
 url += f'ele{ano}/{id_eleicao}/dados-simplificados/{sigla}/'
 url += f'{sigla}-c{cargo}-e000544-r.json'
 
-dados_retorno = requests.get(url).json()
 
-candidatos = dados_retorno['cand'] # separando as informações importantes
+dados_retorno = requests.get(url).json() # Buscando dados na rede.
 
-# pegando apenas as informações pedidas (nome, partido, n° de votos e porcentagem)
+candidatos = dados_retorno['cand'] # Separando as informações importantes.
+
+# Pegando apenas as informações pedidas (nome, partido, n° de votos e porcentagem).
 dicionario = dict()
 for i in candidatos: 
     d = [i['n'],i['nm'],i['cc'],i['vap'],i['pvap']]
     dicionario[d[0]] = {'nome': d[1],'partido': d[2],'votos': d[3],'porcentagem': d[4],}
 
+# Apresentando os dados.
 for i in dicionario:
-    print(i)
-    print(dicionario[i])
+    print(f'{i}: {dicionario[i]}')
+
+DIRETORIO  = os.path.dirname(os.path.abspath(__file__))
+DIRETORIO += '\\pasta_tse\\'
+
+# Criando pasta para o arquivo.
+
+try:
+    os.mkdir(DIRETORIO)
+except FileExistsError:
+    print('O diretóro já existe\n')
+except:
+    print(f'ERRO...:{sys.exc_info()[0]}')
+else:
+    print(f'Diretório {DIRETORIO.upper()} criado com sucesso!!!\n') 
+
+
+# Criando arquivo.
+try:
+    dados_escrita = open(DIRETORIO + 'dados_tse.txt', 'w', encoding='utf-8')
+except:
+    print(f'ERRO...:{sys.exc_info[0]}')
+else:
+    dados_escrita.write('numero;nome,partido;quantidade_votos;percentual_votos\n')
+    for i in dicionario:
+        dados_escrita.write(f'{i}: {dicionario[i]}\n')
+    dados_escrita.close()
+    print('Arquivo criado com sucesso!!!')
