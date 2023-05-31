ca80e769ac1b20b72cb60d6db712b3d74fc2b780
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2023-05-31 11:56:20 -0300

    Update socket_imagem.py

--- 2S - Programação para Redes/socket_imagem.py
+++ 2S - Programação para Redes/socket_imagem.py
@@ -4,7 +4,7 @@ import socket, sys, ssl
 
 url = input('Informe a URL: ')
 
-# Manipulando a string para poder pegar os dados necessários.
+# Manipulando a url/string para poder pegar os dados necessários.
 seacher0     = url.find(':')
 seacher1     = url.find('//')
 url0         = url[seacher1 + 2:]
@@ -12,13 +12,16 @@ seacher2     = url0.find('/')
 reversed_url = url[::-1]
 seacher3     = reversed_url.find('/')
 reversed_url = reversed_url[:seacher3]
+finder_type  = reversed_url[::-1]
+finder_type  = finder_type.split('.')
+replace_txt  = finder_type[-1]
 
 # Dados que serão usados.
 protocol  = url[:seacher0]
 url_host  = url0[:seacher2]
 url_image = url0[seacher2:]
 arq_image = reversed_url[::-1]
-txt_image = arq_image.replace('png','txt')
+txt_image = arq_image.replace(replace_txt,'txt')
 
 print(f'Protocolo...........: {protocol}')
 print(f'URL do host.........: {url_host}')
@@ -26,27 +29,40 @@ print(f'URL da imagem.......: {url_image}')
 print(f'Arquivo da imagem...: {arq_image}')
 print(f'Texto da imagem.....: {txt_image}')
 
-url_request = f'GET {url_image} HTTP/1.1\r\nHOST: {url_host}\r\n\r\n'
-
-sock_img = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-if protocol == 'http':
-    host_port   = 80
-    sock_img.connect((url_host, host_port))
-    sock_img.sendall(url_request.encode())
-elif protocol == 'https':
-    host_port = 443
-    context   = ssl.create_default_context()
-    context.check_hostname = False
-    context.verify_mode    = ssl.CERT_NONE
-    socket_rss = socket.create_connection((url_host, host_port))
-    sock_img   = context.wrap_socket(socket_rss, server_hostname=url_host)
-    sock_img.send(url_request.encode())
-else:
-    print('HÁ ALGO ERRRADO NA URL!!!')
+#-------------------------------------------------------------------------------------
+# Verificando o protocolo. Disponivel: HTTP e HTTPS.
+if protocol != 'http' and protocol != 'https':
+    print('\n')
+    print(100 * '-')
+    print('Apenas os protocolos HTTP e HTTPS são aceitos.')
+    print(f'Protocolo da url informada: {protocol}')
+    print('Os demais protocolos serão adicionados em breve.')
+    print(100 * '-')
     sys.exit()
 
-buffer_size = 1024
+#-------------------------------------------------------------------------------------
+# Criando conexão
+try: 
+    url_request = f'GET {url_image} HTTP/1.1\r\nHOST: {url_host}\r\n\r\n'
+    sock_img = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    if protocol == 'http':
+        host_port   = 80
+        sock_img.connect((url_host, host_port))
+        sock_img.sendall(url_request.encode())
+    elif protocol == 'https':
+        host_port = 443
+        context   = ssl.create_default_context()
+        context.check_hostname = False
+        context.verify_mode    = ssl.CERT_NONE
+        socket_rss = socket.create_connection((url_host, host_port))
+        sock_img   = context.wrap_socket(socket_rss, server_hostname=url_host)
+        sock_img.send(url_request.encode())
+    buffer_size = 1024
+except:
+    print('Erro ao criar a conexão')
+    print(print(f'ERRO...:{sys.exc_info()[0]}'))
+    sys.exit()
 
 print('\nBaixando a imagem...')
 # Montado a variável que armazenará os dados de retorno
@@ -79,5 +95,10 @@ file_output.write(image)
 file_output.close()
 
 # Escrevendo arquivo com os dados do header.
-with open(f'dados_header({url_host}).txt','w',encoding='utf-8') as writing_header:
-    writing_header.write(headers.decode('utf-8'))
+try:
+    with open(f'{txt_image}.txt','w',encoding='utf-8') as writing_header:
+        writing_header.write(headers.decode('utf-8'))
+except:
+    print('Erro ao salvar o arquivo.')
+    print(print(f'ERRO...:{sys.exc_info()[0]}'))
+    sys.exit()
