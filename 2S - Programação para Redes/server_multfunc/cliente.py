78226f7f1b76628e26556b8e61da707e84d0798e
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2023-07-23 01:51:01 -0300

    Update cliente.py

--- 2S - Programação para Redes/server_multfunc/cliente.py
+++ 2S - Programação para Redes/server_multfunc/cliente.py
@@ -1,8 +1,19 @@
-import socket, sys
+import socket, sys, os
 
-HOST = 'localhost'
-PORT = 50000
-CODF = 'utf-8'
+# ===================================================
+SMALL_BF  = 1024
+BIG_BF    = 10240
+HOST      = 'localhost'
+PORT      = 50000
+TRADUCAO  = 'utf-8'
+DIRETORIO = os.path.dirname(os.path.abspath(__file__))
+CLIENT_FL = DIRETORIO + '\\client_file\\'
+# ===================================================
+
+try:    os.mkdir(CLIENT_FL)    
+except  FileExistsError: print('\nO diretóro já existe.')
+except: print(f'\nERRO...:{sys.exc_info()[0]}')
+else:   print(f'\nDiretório criado com sucesso!!!') 
 
 # Criando socket.
 conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,17 +22,35 @@ conn.connect((HOST, PORT))
 
 while True:
     pedido  = input('>')
-    conn.send(pedido.encode(CODF))
+    conn.send(pedido.encode(TRADUCAO))
     servico = pedido[:2]
 
+    # Sair do servidor.
     if servico == '/q':
         print('DESLOGADO DO SERVIDOR')
         sys.exit()
     
+    # Lista de IPs.
     elif servico == '/l':
-        ips = conn.recv(1024).decode(CODF)
+        ips = conn.recv(SMALL_BF).decode(TRADUCAO)
         ips = ips.split('/')
         for ip in ips: print(ip)
     
+    # Lista de arquivos do servidor.
     elif servico == '/f':
-        arq = conn.recv(1024).decode(CODF)
+        pacotes_f = int(conn.recv(SMALL_BF).decode(TRADUCAO))
+        arquivos  = ''
+        for loop in range(pacotes_f): arquivos += conn.recv(SMALL_BF).decode(TRADUCAO)
+        arquivos = arquivos.split('/')
+        for arquivo in arquivos: print(arquivo)
+    
+    elif servico == '/d':
+        tamanho_arq = int(conn.recv(SMALL_BF))
+        dados_recv  = 0
+        with open(CLIENT_FL + pedido.split(':')[-1], 'wb') as file:
+            while dados_recv < tamanho_arq:
+                data = conn.recv(BIG_BF)
+                file.write(data)
+                dados_recv += len(data)
+                sys.stdout.write(f'\r {dados_recv}/{tamanho_arq} bytes recebidos')
+                sys.stdout.flush()
