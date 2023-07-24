e0f5c1754837b95d9fa89c0c8f21bcd01eb219ab
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2023-07-23 23:14:34 -0300

    Update cliente.py

--- 2S - Programação para Redes/server_multfunc/cliente.py
+++ 2S - Programação para Redes/server_multfunc/cliente.py
@@ -1,4 +1,4 @@
-import socket, sys, os, threading
+import socket, sys, os, threading, time
 
 # ===================================================
 SMALL_BF  = 1024
@@ -11,13 +11,13 @@ CLIENT_FL = DIRETORIO + '\\client_file\\'
 # ===================================================
 
 try:    os.mkdir(CLIENT_FL)    
-except  FileExistsError: print('\nO diretóro já existe.')
+except  FileExistsError: print('\nO diretório já existe.')
 except: print(f'\nERRO...:{sys.exc_info()[0]}')
 else:   print(f'\nDiretório criado com sucesso!!!') 
 
 # Criando socket.
 conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# Conectando ao servidor
+# Conectando ao servidor.
 conn.connect((HOST, PORT))
 
 while True:
@@ -25,8 +25,13 @@ while True:
     conn.send(pedido.encode(TRADUCAO))
     servico = pedido[:2]
 
+    # Lista de comandos.
+    if servico == '/?':
+        comandos = eval((conn.recv(SMALL_BF)).decode(TRADUCAO))
+        for comando in comandos: print(comando)
+
     # Sair do servidor.
-    if servico == '/q':
+    elif servico == '/q':
         print('DESLOGADO DO SERVIDOR')
         sys.exit()
     
@@ -49,7 +54,6 @@ while True:
             try:
                 print('Baixando arquivo...')
                 tamanho_arq = int(conn.recv(SMALL_BF).decode(TRADUCAO))
-                print(tamanho_arq)
                 dados_recv  = 0
                 with open(CLIENT_FL + pedido.split(':')[-1], 'wb') as file:
                     while dados_recv < tamanho_arq:
@@ -64,4 +68,20 @@ while True:
         else:
             tDOWNLOAD = threading.Thread(target=download_file)
             tDOWNLOAD.start()
-            
+    
+    elif servico == '/u':
+        def upload_file():
+            try:
+                file_path = CLIENT_FL + pedido.split(':')[-1]
+                tamanho_u = str(os.path.getsize(file_path))
+                conn.send(((tamanho_u).encode(TRADUCAO)))
+                time.sleep(2)
+                while True:
+                    with open(file_path, 'rb') as file:
+                        data = file.read(BIG_BF)
+                        if not data: break
+                        conn.send(data)
+            except: print(f'\nERRO...:{sys.exc_info()}')
+            else: print(f'Upload concluído: {pedido.split(":")[-1]}')
+        tUPLOAD = threading.Thread(target=upload_file)
+        tUPLOAD.start()
