4183a867e896fcd5d7177e5bd6e9102de22bb6e0
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2023-07-29 12:59:22 -0300

    Update cliente.py

--- 2S - Programação para Redes/server_multfunc/cliente.py
+++ 2S - Programação para Redes/server_multfunc/cliente.py
@@ -1,8 +1,8 @@
-import socket, sys, os, time
+import socket, sys, os, time, threading
 
 # lista de arquivos
 
-# ===================================================
+# ================================ ÁREA DE CONSTANTES ==================================
 SMALL_BF  = 1024
 BIG_BF    = 10240
 HOST      = 'localhost'
@@ -10,87 +10,74 @@ PORT      = 50000
 TRADUCAO  = 'utf-8'
 DIRETORIO = os.path.dirname(os.path.abspath(__file__))
 CLIENT_FL = DIRETORIO + '\\client_file\\'
-# ===================================================
+# ================================== ÁREA DE FUNÇÕES ===================================
 
-try:    os.mkdir(CLIENT_FL)    
-except  FileExistsError: print('\nO diretório já existe.')
-except: print(f'\nERRO...:{sys.exc_info()[0]}')
-else:   print(f'\nDiretório criado com sucesso!!!') 
+def receptor():
+        global runnig
+        while runnig:
+            recp_data = conn.recv(SMALL_BF).decode(TRADUCAO)
+            data = recp_data[2:]
+            if recp_data[0] == '?':
+                comandos = eval(recp_data[2:])
+                for comando in comandos: print(comando)
+
+            elif recp_data[0] == 'l':
+                ips = recp_data[2:].split('/')
+                for ip in ips: print(ip)
+            
+            elif recp_data[0] == 'm':
+                print(recp_data[2:])
+            
+            elif recp_data[0] == 'f':
+                pacotes_f = int(recp_data[2:])
+                arquivos  = ''
+                for loop in range(pacotes_f): arquivos += conn.recv(SMALL_BF).decode(TRADUCAO)
+                arquivos = arquivos.split('/')
+                for arquivo in arquivos: print(arquivo)
+                 
+
+            elif recp_data[0] == 'd':
+                try:
+                    arquivo_d = pedido.split(':')[-1]
+                    print(f'Baixando arquivo: {arquivo_d}')
+                    tamanho_arq = int(conn.recv(SMALL_BF).decode(TRADUCAO))
+                    dados_recv  = 0
+                    with open(CLIENT_FL + arquivo_d, 'wb') as file:
+                        while dados_recv < tamanho_arq:
+                            data = conn.recv(BIG_BF)
+                            file.write(data)
+                            dados_recv += len(data)
+                            sys.stdout.write(f'\rBytes recebidos: {dados_recv}/{tamanho_arq}')
+                            sys.stdout.flush()
+                except: print(f'ERRO...:{sys.exc_info()}')
+                else: print('\nDownload concluído.')
+
+# =======================================================================================
 
 # Criando socket.
 conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Conectando ao servidor.
 conn.connect((HOST, PORT))
 
+try:    os.mkdir(CLIENT_FL)    
+except  FileExistsError: print('\nO diretório já existe.')
+except: print(f'\nERRO...:{sys.exc_info()[0]}')
+else:   print(f'\nDiretório criado com sucesso!!!') 
+
+# Ativando receptor.
+runnig = True
+tRECEPTOR = threading.Thread(target=receptor)
+tRECEPTOR.start()
+
+# Envio de comandos.
 while True:
-    pedido  = input('>')
-    servico = pedido[:2]
-
-    if servico == '/u':
-        len_file = str(os.path.getsize(CLIENT_FL + pedido.split(':')[-1]))
-        pedido += f':{len_file}'
+    pedido = input('\n>')
     conn.send(pedido.encode(TRADUCAO))
-
-    # Lista de comandos.
-    if servico == '/?':
-        comandos = eval((conn.recv(SMALL_BF)).decode(TRADUCAO))
-        for comando in comandos: print(comando)
+    time.sleep(0.5)
 
     # Sair do servidor.
-    elif servico == '/q':
-        print('DESLOGADO DO SERVIDOR')
+    if pedido [:2] == '/q':
+        runnig = False
+        print('\nDESLOGADO DO SERVIDOR')
         sys.exit()
-    
-    # Lista de IPs.
-    elif servico == '/l':
-        ips = conn.recv(SMALL_BF).decode(TRADUCAO)
-        ips = ips.split('/')
-        for ip in ips: print(ip)
-    
-    # Lista de arquivos do servidor.
-    elif servico == '/f':
-        pacotes_f = int(conn.recv(SMALL_BF).decode(TRADUCAO))
-        arquivos  = ''
-        for loop in range(pacotes_f): arquivos += conn.recv(SMALL_BF).decode(TRADUCAO)
-        arquivos = arquivos.split('/')
-        for arquivo in arquivos: print(arquivo)
-    # Download de arquivos.
-    elif servico == '/d':
-        try:
-            arquivo_d = pedido.split(':')[-1]
-            arq_lista = os.listdir(CLIENT_FL)
-            if arquivo_d in arq_lista:
-                num = 1
-                file_name = arquivo_d.split('.')[0]
-                file_type = arquivo_d.split('.')[1]
-                file_name += f'({num})'
-                arquivo_d = file_name + '.' + file_type
-                while arquivo_d in arq_lista:
-                    num2 = num + 1
-                    arquivo_d.replace(str(num), str(num2))
-                    print(arquivo_d)
-                    num += 1
-            print(f'Baixando arquivo: {arquivo_d}')
-            tamanho_arq = int(conn.recv(SMALL_BF).decode(TRADUCAO))
-            dados_recv  = 0
-            with open(CLIENT_FL + arquivo_d, 'wb') as file:
-                while dados_recv < tamanho_arq:
-                    data = conn.recv(BIG_BF)
-                    file.write(data)
-                    dados_recv += len(data)
-                    sys.stdout.write(f'\rBytes recebidos: {dados_recv}/{tamanho_arq}')
-                    sys.stdout.flush()
-        except: print(f'ERRO...:{sys.exc_info()}\n')
-        else: print('Download concluído.\n')
-            
-    elif servico == '/u':
-        try:
-            file_path = CLIENT_FL + pedido.split(':')[1]
-            time.sleep(2)
-            with open(file_path, 'rb') as file:
-                while True: 
-                    data = file.read(BIG_BF)
-                    if not data: break
-                    conn.send(data)
-        except: print(f'ERRO...:{sys.exc_info()}\n')
-        else: print(f'Upload concluído: {pedido.split(":")[1]}\n')
+        
