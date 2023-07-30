26cb46357bcf5a2661887443c19b78d9a59796d2
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2023-07-30 12:28:55 -0300

    Update servidor.py

--- 2S - Programação para Redes/server_multfunc/servidor.py
+++ 2S - Programação para Redes/server_multfunc/servidor.py
@@ -1,4 +1,4 @@
-import socket, threading, sys, requests, os, datetime, math, time, ssl
+import socket, threading, sys, requests, os, datetime, math, ssl
 from chave_api import API_KEY
 
 # verificador de repetição das fotos
@@ -37,6 +37,18 @@ def telegram(logg):
     mensagem   = logg
     resposta   = {'chat_id':id_chat,'text':mensagem}
     envio      = requests.post(url_req + '/sendMessage', data=resposta)
+
+# Verificador de repetição de nome.
+def repeticao(name_img):
+    nome, tipo = os.path.splitext(name_img)
+    lis = os.listdir(DIRETORIO)
+    contador = 1
+    while True:
+        name_img = nome + f'({contador})' + tipo
+        if not name_img in lis: break
+        contador += 1
+        name_img = ''
+    return name_img
 
 # /?: Lista de comandos.
 def lista_comandos(conexao, cliente):
@@ -127,6 +139,97 @@ def download(conexao, cliente, entrada_comando):
         print(f'{cliente} > "/d" - {arquivo_d}')
         ATIVIDADE.append(f'{str(datetime.datetime.now())}|"/d":{cliente}|{arquivo_d}')
 
+# /w: Download da web.
+def web_download(conexao, cliente, entrada_comando): 
+    try:
+        url  = entrada_comando[3:]
+        aux1 = url.find('//')           # Auxiliar de procura 1.
+        url1 = url[aux1 + 2:]           # URL sem o protocolo e //.
+        aux2 = url1.find('/')           # Auxiliar de procura 2.
+
+        # Dados extraídos da URL.
+        protocol  = url.split(':')[0]   # Protocolo.
+        url_image = url1[aux2:]         # Caminho e nome da imagem.
+        url_host  = url1.split('/')[0]  # Nome do host.
+        name_img  = url1.split('/')[-1]
+        name_img  = name_img.split('.')[0]
+        
+        url_request = f'GET {url_image} HTTP/1.1\r\nHOST: {url_host}\r\nConnection:close\r\n\r\n'
+        sock_img = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                
+                # Verificando o protocolo.
+        if protocol != 'http' and protocol != 'https':
+            print('protocolo errado.')
+            #conexao.send((f'w:Download penas de imagens com os protocolos HTTP ou HTTPS. Protocolo: {protocol}').encode(TRADUCAO))
+        else:
+            if protocol == 'http':
+                host_port   = 80
+                sock_img.connect((url_host, host_port))
+                sock_img.sendall(url_request.encode())
+            elif protocol == 'https':
+                host_port = 443
+                context   = ssl.create_default_context()
+                context.check_hostname = False
+                context.verify_mode    = ssl.CERT_NONE
+                socket_rss = socket.create_connection((url_host, host_port))
+                sock_img   = context.wrap_socket(socket_rss, server_hostname=url_host)
+                sock_img.send(url_request.encode())
+
+        print('Baixando a imagem...')
+        # Montado a variável que armazenará os dados de retorno
+        data_ret = ''.encode()
+        while True:
+            data = sock_img.recv(1024)
+            if not data: break
+            data_ret += data
+
+        # Obtendo o tamanho da imagem e o tipo de aquivo.
+        type_name = ''
+        tmp = data_ret.split('\r\n'.encode())
+        for line in tmp:
+            # Tipo de arquivo.
+            if 'Content-Type:'.encode() in line:
+                type_name = str(line.split()[1])
+                break
+
+        # Dados que serão usados.
+        type_name  = type_name.split('/')[1]
+        type_name  = type_name.replace("'","")
+        name_img += f'.{type_name}'
+
+        # Separando o Cabeçalho dos Dados
+        delimiter = '\r\n\r\n'.encode()
+        position  = data_ret.find(delimiter)
+        image     = data_ret[position + 4:]
+
+        # Verificando repetição de nomes.
+        name_img = repeticao(name_img)
+
+        # Salvando a imagem
+        file_output = open(SERVER_FILES + f'{name_img}', 'wb')
+        file_output.write(image)
+        file_output.close()
+
+        try:
+            conexao.send((f'w1:{name_img}').encode(TRADUCAO))
+            tamanho_d = str(os.path.getsize(SERVER_FILES + name_img))
+            conexao.send(tamanho_d.encode(TRADUCAO))
+            with open(SERVER_FILES + name_img, 'rb') as file:
+                while True:
+                    data = file.read(BIG_BUFFER)
+                    if not data: break
+                    conexao.send(data)
+        except:
+            print(f'{cliente}: Envio interrompido > {sys.exc_info()[0]}')
+            ATIVIDADE.append(f'{str(datetime.datetime.now())}|"/w":{cliente}|{name_img}>>{sys.exc_info()[0]}')
+        else:
+            print(f'{cliente} > "/w" - {name_img}')
+            ATIVIDADE.append(f'{str(datetime.datetime.now())}|"/w":{cliente}|{name_img}')
+    except:
+        print(f'Erro ao baixar o arquivo {name_img}: {sys.exc_info()}')
+        conexao.send((str(f'w:Erro ao baixar o arquivo {name_img}: {sys.exc_info()[0]}')).encode(TRADUCAO))
+        ATIVIDADE.append(f'{str(datetime.datetime.now())}|"/w":{cliente}|{name_img}|ERRO:{sys.exc_info()[0]}|{url}')
+
 # ============================== Thread onde o cliente faz a interação com o servidor =====================================
 
 def servicos(conexao, cliente):
@@ -191,6 +294,11 @@ def servicos(conexao, cliente):
             elif serv == '/d':
                 tDOWNLOAD = threading.Thread(target=download, args=(conexao, cliente, entrada_comando,))
                 tDOWNLOAD.start()
+            
+            # Download da web.
+            elif serv == '/w':
+                tWEB = threading.Thread(target=web_download, args=(conexao, cliente, entrada_comando,))
+                tWEB.start()
            
         except ConnectionResetError:
             print(f'\nO cliente "{cliente}" deslogou abruptamente!!!\n')
