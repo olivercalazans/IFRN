67413e268d715aba576240016712066feee373c8
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2023-08-02 14:44:19 -0300

    Update 1D.sh

---
+++ 2S - Intordução aos Sistemas Abertos/1D.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Informe o nome do arquivo"
+read nome_arquivo
+
+caminho="$HOME/$nome_arquivo"
+
+if [ -e "$caminho" ]; then
+    echo "Esse nome já existe"
+else
+    touch "$nome_arquivo"
+    chmod 444 "$nome_arquivo"
+    echo "Arquivi criado com sucesso"
+fi
