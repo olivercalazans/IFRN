8181a483e9a3850cfca258d911c3b3f31c0fb47c
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2023-12-14 16:26:58 -0300

    Create visualizando_usuarios_ssh.sh

---
+++ 3S - Introdução aos Sistemas Abertos/visualizando_usuarios.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+IFS=$'\n'
+
+echo
+
+for linha in `who | grep -v tty`; do
+  nome=$(echo $linha | awk '{print $1}')
+  terminal=$(echo $linha | awk '{print $2}')
+  data=$(echo $linha | awk '{print $3}')
+  hora=$(echo $linha | awk '{print $4}')
+  ip=$(echo $linha | awk '{print $5}')
+
+  echo "Usuário....: $nome"
+  echo "Terminal...: $terminal"
+  echo "Data.......: $data $hora"
+  echo "IP.........: $ip"
+  echo
+done
