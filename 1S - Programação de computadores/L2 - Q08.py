e3261e2bf94c2f25993971d20e1dd28cf5dede18
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2024-01-13 00:02:24 -0300

    Correção ortográfica.

--- 1S - Programação de computadores/L2 - Q08.py
+++ 1S - Programação de computadores/L2 - Q08.py
@@ -6,10 +6,10 @@ delta = b**2 - 4 * a * c
 
 if delta == 0:
     x = ((-b) + delta**(1/2)) / 2 * a
-    print(f'O delta de A, B e C é zero, então só existe uma raíz que é {x}.')
+    print(f'O delta de A, B e C é zero. Então só existe uma raíz que é {x}.')
 elif delta > 0:
     x = ((-b) + delta**(1/2)) / 2 * a
     x2 = ((-b) - delta**(1/2)) / 2 * a
     print(f'As raízes são {x} e {x2}.')
 else:
-    print('O delta é menor que zero, então não existe solução real.')
+    print('O delta é menor que zero. Então não existe solução real.')
